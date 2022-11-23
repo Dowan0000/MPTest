1ceb8fcf03e24116b5f1bf63ca11a4ff1f331fe7
Dowan <shukang91@gmail.com>
2022-11-23 17:43:55 +0900

    11.23 weapon

---
+++ Source/MPTest/Weapon.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon.h"
+#include "Components/BoxComponent.h"
+#include "ShooterCharacter.h"
+#include "Engine/SkeletalMeshSocket.h"
+
+// Sets default values
+AWeapon::AWeapon()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	bReplicates = true;
+
+	Box = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
+	
+	RootComponent = Box;
+	Mesh->SetupAttachment(RootComponent);
+
+	Box->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	Box->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+
+	Mesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
+	Mesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+	Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+}
+
+// Called when the game starts or when spawned
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Box->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::BoxBeginOverlap);
+}
+
+// Called every frame
+void AWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AWeapon::BoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AShooterCharacter* Character = Cast<AShooterCharacter>(OtherActor);
+	if (Character)
+	{
+		const USkeletalMeshSocket* SocketName = Character->GetMesh()->GetSocketByName(FName("hand_r_socket"));
+		if (SocketName)
+		{
+			SocketName->AttachActor(this, Character->GetMesh());
+		}
+	}
+}
---
+++ Source/MPTest/Weapon.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UCLASS()
+class MPTEST_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AWeapon();
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UFUNCTION()
+	void BoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+	class UBoxComponent* Box;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+	class USkeletalMeshComponent* Mesh;
+
+
+public:	
+	
+
+};
