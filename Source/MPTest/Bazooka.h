3607a29689390eb99bdc7efc665c7687bb32cb35
Dowan <shukang91@gmail.com>
2022-12-04 16:02:21 +0900

    12/04 bazooka 에셋, 클래스생성

---
+++ Source/MPTest/Bazooka.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon.h"
+#include "Bazooka.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MPTEST_API ABazooka : public AWeapon
+{
+	GENERATED_BODY()
+	
+};
